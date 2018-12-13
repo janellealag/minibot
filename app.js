18715fa3d52b769b161c4670849d268b604f1f5c
Janelle <imjanellealag@gmail.com>
2018-12-13 17:25:00 +0800

    chat added

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
 });
 
-var nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket) {
-   console.log('someone connected');
-   nsp.emit('hi', 'Hello everyone!');
+users = [];
+io.on('connection', function (socket) {
+    console.log('A user connected');
+    socket.on('setUsername', function (data) {
+        console.log(data);
+
+        if (users.indexOf(data) > -1) {
+            socket.emit('userExists', data + ' username is taken! Try some other username.');
+        } else {
+           // users.push(data);
+            socket.emit('userSet', { username: data });
+        }
+    });
+
+    socket.on('msg', function (data) {
+        //Send message to everyone
+        io.sockets.emit('newmsg', data);
+    });
+
+    socket.on('disconnect', function () {
+       
+    })
+
+    socket.on('joinRoom', function (data) {
+        io.sockets.in("nec").emit('connectToRoom');
+    })
 });
 
-http.listen(3000, function() {
-   console.log('listening on localhost:3000');
+http.listen(3000, function () {
+    console.log('listening on localhost:3000');
 });
